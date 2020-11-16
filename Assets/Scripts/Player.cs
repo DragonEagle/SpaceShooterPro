9ca0091aea1b7ae21d7b131c4eda563ebfa03ad2
DragonEagle <allensoard@yahoo.com>
2020-11-16 02:47:25 -0800

    Added player movement.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  Take the current position == Start position (0,0,0)
+        transform.position = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 moveVector = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(moveVector * _speed * Time.deltaTime);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
+        if (transform.position.y < -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+        else if (transform.position.y > 0f)
+        {
+            transform.position = new Vector3(transform.position.x, 0f, 0);
+        }
+    }
+}

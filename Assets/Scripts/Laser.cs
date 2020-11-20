2e7426dad3422e4f9804c299b095c7e256486957
DragonEagle <allensoard@yahoo.com>
2020-11-20 14:17:26 -0800

    Changed the laser's direction vector to transform.up in prepraration for adding scatter shot

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -9,7 +9,7 @@ public class Laser : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+        transform.Translate(transform.up * _speed * Time.deltaTime);
         if (transform.position.y > 8f)
         {
             if(transform.parent != null)
